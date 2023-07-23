b51358165a12123b014b52472e7136d3d20b2787
Ramon Oliveira <ramon.braga.o1997@gmail.com>
2023-07-23 19:11:12 -0400

    complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...values) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < values.length; j++) {
+            if (arr[i] === values[j]) {
+                arr.splice(i, 1)
+                i--
+            }
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
