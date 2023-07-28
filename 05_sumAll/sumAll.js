a5bed47f708b12a08bf84e138bcc8358f0606cf3
Ramon Oliveira <ramon.braga.o1997@gmail.com>
2023-07-28 09:43:53 -0400

    complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (typeof num1 == 'number' && typeof num2 == 'number' && num1 >= 0 && num2 >= 0) {
+        if (num1 > num2) {
+            let temp = num1
+            num1 = num2
+            num2 = temp
+        }
+        let result = num1
+        for (let i = num1 + 1; i <= num2; i++) {
+            result += i
+        }
 
+        return result
+    } else return 'ERROR'
+    
 };
 
 // Do not edit below this line
