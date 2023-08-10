092d4a3ccd2a15ee436da43753c1ccfe54136476
Ramon Oliveira <ramon.braga.o1997@gmail.com>
2023-08-10 10:39:21 -0400

    solve 08_calculator problem

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+  }
+  return parseInt(sum)
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let currentNum = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    currentNum = currentNum * nums[i]
+  }
+  return currentNum
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0 || a === 1) {
+    return 1
+  }
+  let n = a
+	for (let i = 1; i < a; i++) {
+    n = n * (a - i);
+  }
+  return n
 };
 
 // Do not edit below this line
