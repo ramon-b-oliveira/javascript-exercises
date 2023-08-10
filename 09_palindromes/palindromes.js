27edf13c30365e0d3882e7c0b588eb7be190baa6
Ramon Oliveira <ramon.braga.o1997@gmail.com>
2023-08-10 11:02:49 -0400

    solve problem 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase()
+    let arr = Array.from(str);
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === " " || arr[i] === "," || arr[i] === "." || arr[i] === "!") {
+            arr.splice(i, 1);
+            i--
+        }
+    }
+    
+    let reversedArr = arr.slice().reverse();
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== reversedArr[i]) {
+            return false
+        }
+    }
 
+    return true
 };
 
 // Do not edit below this line
