3e53ca33abd70af0c2c410f89089011baeb27530
Ramon Oliveira <ramon.braga.o1997@gmail.com>
2023-08-10 12:31:03 -0400

    solve problem 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let rightPerson = {}
+    let ageCounter = 0
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].hasOwnProperty("yearOfDeath")) {
+            let personAge = arr[i].yearOfDeath - arr[i].yearOfBirth
+            if (personAge > ageCounter) {
+                ageCounter = personAge
+                rightPerson = arr[i]
+            }
+        } else {
+            let personAge = new Date().getFullYear() - arr[i].yearOfBirth
+            if (personAge > ageCounter) {
+                ageCounter = personAge
+                rightPerson = arr[i]
+            }
+        }
+    }
+    return rightPerson
 };
 
 // Do not edit below this line
