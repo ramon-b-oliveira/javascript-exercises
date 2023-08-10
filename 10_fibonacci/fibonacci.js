5ec48533d15b292511db908b722e81e0d6a63090
Ramon Oliveira <ramon.braga.o1997@gmail.com>
2023-08-10 11:27:25 -0400

    solve problem 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = parseInt(num)
+    if (num < 0) {
+        return "OOPS"
+    }
+    let fibo = [];
 
+    for (let i = 0; i < num; i++) {
+        if (i <= 1) {
+            fibo.push(1)
+        } else {
+            fibo.push((fibo[fibo.length - 1] + fibo[fibo.length - 2]))
+        }
+    }
+    return fibo[fibo.length - 1]
 };
 
 // Do not edit below this line
